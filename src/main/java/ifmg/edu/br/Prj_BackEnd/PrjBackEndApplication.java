7519ef8135c61c0531b9607493a186b478bf48c6
AndreCA1 <carvalhoandre149@gmail.com>
2025-03-27 12:56:18 -0300

    Criando repozitório

---
+++ src/main/java/ifmg/edu/br/Prj_BackEnd/PrjBackEndApplication.java
@@ -0,0 +1,13 @@
+package ifmg.edu.br.Prj_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ifmg/edu/br/Prj_BackEnd/PrjBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package ifmg.edu.br.Prj_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
